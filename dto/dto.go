453b445cc3e03b7ddb128ee90246b4cbf2beaf2e
linchunbo <linchunbo@foxmail.com>
2024-07-02 17:02:23 +0800

    sdk调整

--- dto/dto.go
+++ dto/dto.go
@@ -2,11 +2,11 @@ package dto
 
 import "time"
 
-// AppOpen 请求统计为密文传输
+// 请求统计为密文传输
 type AppOpenReq struct {
-	Version string `json:"version" form:"version"` //版本
-	Encrypt string `json:"encrypt" form:"encrypt"` //加密方式 aes,rsa（版本2以上提供，默认aes,老接口rsa）aes cbc模式
-	AppKey  string `json:"appKey" form:"appKey"`   //appKey
+	Version string `json:"version" form:"version"` // 版本 v2
+	Encrypt string `json:"encrypt" form:"encrypt"` // 加密方式 aes,rsa（版本2以上提供，默认aes,老接口rsa）aes cbc模式
+	AppKey  string `json:"appKey" form:"appKey"`   // appKey
 	Params  string `json:"params" form:"params"`   // 根据加密方式密文 转base64
 }
 
@@ -19,43 +19,42 @@ type Res struct {
 
 // 获取产品列表请求
 type AppProductSyncReq struct {
-	ProxyType []int `json:"proxyType" form:"proxyType"` //代理类型 101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
+	ProxyType []int `json:"proxyType" form:"proxyType"` //代理类型  101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
 }
 
 // 获取产品列表返回 （明文返回）
 type AppProductSyncResp struct {
-	ProductNo      string  `json:"productNo" form:"productNo"`     //必要，产品Id 保持唯一
-	ProductName    string  `json:"productName" form:"productName"` //必要,商品名
-	ProxyType      int16   `json:"proxyType" form:"proxyType"`     //必要, 101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
-	UseType        string  `json:"useType" form:"useType"`         //必要, ,分割  1=账密 2=白名单 3=uuid
-	Protocol       string  `json:"protocol" form:"protocol"`       //必要,1=socks5 2=http 3=https 4=ssh
-	UseLimit       int8    `json:"useLimit" form:"useLimit"`       //必要,1=出口ip国外 2=出口ip国内 3=无限制
-	SellLimit      int8    `json:"sellLimit" form:"sellLimit"`     //必要,1=大陆可售 2=海外可售 3=无限制
-	AreaCode       string  `json:"areaCode" form:"areaCode"`       //区域code
-	CountryCode    string  `json:"countryCode" form:"countryCode"` //必要,国家代码 3位 iso标准
-	CityCode       string  `json:"cityCode" form:"cityCode"`       //必要,城市代码 向我方提取
-	Detail         string  `json:"detail" form:"detail"`           //商品描述
-	CostPrice      string  `json:"costPrice" `                     //必要 价格
-	Inventory      int     `json:"inventory"`                      //必要 库存
-	IpType         int     `json:"ipType" `                        //ip类型 1=ipv4 2=ipv6 3=随机 默认1
-	IspType        int     `json:"ispType" `                       //ispType 1=单isp 2=双isp
-	Duration       int     `json:"duration"`                       //必要 时长 0无限制
-	Unit           int     `json:"unit" `                          //单位 1=天 2=周(7天) 3=月(自然月) 4=年(自然年365，366)
-	BandWidth      int     `json:"bandWidth"`                      //带宽|流量时必要 单位 MB
-	BandWidthType  int     `json:"bandWidthType" `                 //带宽类型 1=独享 2=共享 3=流量包
-	BandWidthPrice string  `json:"bandWidthPrice" `                //额外带宽价格
-	MaxBandWidth   int     `json:"maxBandWidth" `                  //可设置最大带宽
-	Flow           int     `json:"flow"`                           //流量包
-	Cpu            int     `json:"cpu"`                            //cpu数
-	Memory         float64 `json:"memory"`                         //内存容量
-	Enable         int8    `json:"enable"`                         //是否可以购买 1可以
-	SupplierCode   string  `json:"supplierCode"`                   //供应商代码
-	//新增 2024/06/27
-	CIDRBlocks []CIDRBlock `json:"cidrBlocks"` //支持网段及数量
+	ProductNo      string      `json:"productNo" form:"productNo"`     //必要，产品Id 保持唯一
+	ProductName    string      `json:"productName" form:"productName"` //必要,商品名
+	ProxyType      int16       `json:"proxyType" form:"proxyType"`     //必要, 101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
+	UseType        string      `json:"useType" form:"useType"`         //必要, ,分割  1=账密 2=白名单 3=uuid
+	Protocol       string      `json:"protocol" form:"protocol"`       //必要,1=socks5 2=http 3=https 4=ssh
+	UseLimit       int8        `json:"useLimit" form:"useLimit"`       //必要,1=出口ip国外 2=出口ip国内 3=无限制
+	SellLimit      int8        `json:"sellLimit" form:"sellLimit"`     //必要,1=大陆可售 2=海外可售 3=无限制
+	AreaCode       string      `json:"areaCode" form:"areaCode"`       //区域code
+	CountryCode    string      `json:"countryCode" form:"countryCode"` //必要,国家代码 3位 iso标准
+	CityCode       string      `json:"cityCode" form:"cityCode"`       //必要,城市代码 向我方提取
+	Detail         string      `json:"detail" form:"detail"`           //商品描述
+	CostPrice      string      `json:"costPrice"`                      //必要 价格
+	Inventory      int         `json:"inventory"`                      //必要 库存
+	IpType         int         `json:"ipType"`                         //ip类型 1=ipv4 2=ipv6 3=随机 默认1
+	IspType        int         `json:"ispType"`                        //ispType 1=单isp 2=双isp
+	Duration       int         `json:"duration"`                       //必要 时长 0无限制
+	Unit           int         `json:"unit"`                           //单位 1=天 2=周(7天) 3=月(自然月) 4=年(自然年365，366)
+	BandWidth      int         `json:"bandWidth"`                      //带宽|流量时必要 单位 MB
+	BandWidthType  int         `json:"bandWidthType"`                  //带宽类型 1=独享 2=共享 3=流量包
+	BandWidthPrice string      `json:"bandWidthPrice"`                 //额外带宽价格
+	MaxBandWidth   int         `json:"maxBandWidth"`                   //可设置最大带宽
+	Flow           int         `json:"flow"`                           //流量包
+	Cpu            int         `json:"cpu"`                            //cpu数
+	Memory         float64     `json:"memory"`                         //内存容量
+	Enable         int8        `json:"enable"`                         //是否可以购买 1可以
+	SupplierCode   string      `json:"supplierCode"`                   //供应商代码
+	CIDRBlocks     []CIDRBlock `json:"cidrBlocks"`                     //支持网段及数量 新增 2024/06/27
 }
 
-//新增 2024/06/27
-//网段
+// 新增 2024/06/27
+// 网段
 type CIDRBlock struct {
 	CIDR  string `json:"cidr"` //网段 192.168.0.0/24 172.16.0.0/16 10.0.0.0/8
 	Count int    `json:"count"`
@@ -85,22 +84,22 @@ type AppCreateUserResp struct {
 
 // 创建或修改代理用户请求
 type AppProxyUserReq struct {
-	AppUsername     string `json:"appUsername" form:"appUsername"`         //渠道商子账号 该渠道商唯一  (不传随机生成) 不支持修改
-	Password        string `json:"password" form:"password"`               //密码(不传随机生成) 不支持修改
+	AppUsername     string `json:"appUsername" form:"appUsername"`         //渠道商子账号 该渠道商唯一 (不传随机生成) 不支持修改
+	Password        string `json:"password" form:"password"`               //密码(不传随机生成)
 	LimitFlow       int    `json:"limitFlow" form:"limitFlow"`             //动态流量上限
-	MainUsername    string `json:"mainUsername" form:"mainUsername"`       //平台主账号 选填 平台主账号和渠道商主账号两个必填一个 不支持修改
-	AppMainUsername string `json:"appMainUsername" form:"appMainUsername"` //渠道商主账号 选填 平台主账号和渠道商主账号两个必填一个 不支持修改
+	MainUsername    string `json:"mainUsername" form:"mainUsername"`       //平台主账号  选填 平台主账号和渠道商主账号两个必填一个
+	AppMainUsername string `json:"appMainUsername" form:"appMainUsername"` //渠道商主账号 选填 平台主账号和渠道商主账号两个必填一个
 	Remark          string `json:"remark" form:"remark"`                   //备注
 	Status          int8   `json:"status" form:"status"`                   //状态 1=正常 2=禁用
 }
 
 // 创建或修改代理用户返回
 type AppProxyUserResp struct {
-	AppUsername string `json:"appUsername" form:"appUsername"` //渠道商子账号
-	Username    string `json:"username" form:"username"`       //平台子账号
-	Password    string `json:"password" form:"password"`       //子账号密码
-	Status      int8   `json:"status" form:"status"`           //用户状态 1=正常 2=禁用
-	AuthStatus  int8   `json:"authStatus" form:"authStatus"`   //认证状态 1=未实名 2=个人实名 3=企业实名
+	AppUsername string `json:"appUsername"` //渠道商子账号
+	Username    string `json:"username"`    //平台子账号
+	Password    string `json:"password"`    //子账号密码
+	Status      int8   `json:"status"`      //用户状态 1=正常 2=禁用
+	AuthStatus  int8   `json:"authStatus"`  //认证状态 1=未实名 2=个人实名 3=企业实名
 }
 
 // 同步实名请求
@@ -115,31 +114,30 @@ type AppAuthUserReq struct {
 
 // 同步实名返回
 type AppAuthUserResp struct {
-	Username   string `json:"username" form:"username"`     //平台账号
-	AuthStatus int    `json:"authStatus" form:"authStatus"` //认证状态 1=未实名 2=个人实名 3=企业实名
+	Username   string `json:"username"`   //平台账号
+	AuthStatus int    `json:"authStatus"` //认证状态 1=未实名 2=个人实名 3=企业实名
 }
 
 // 获取订单列表请求
 type AppGetOrderReq struct {
-	Orders []string `json:"orders" form:"orders"` //订单编号
+	OrderNo  string `json:"orderNo" form:"orderNo"`   // 平台订单编号
+	Page     int    `json:"page" form:"page"`         // 页码  默认1
+	PageSize int    `json:"pageSize" form:"pageSize"` // 每页显示数量  默认10 最大100
 }
 
 // 订单返回
 type AppOrderResp struct {
-	No             string            `json:"no"`             //订单号（系统订单）
-	Type           int8              `json:"type"`           //1=新建 2=续费 3=释放
-	Status         int8              `json:"status"`         //状态 1=待处理 2=处理中 3=处理成功 4=处理失败 5=部分完成
-	Count          int               `json:"count"`          //购买数量
-	CostTotal      string            `json:"costTotal"`      //总成本
-	Amount         string            `json:"amount"`         //总售价
-	Discount       string            `json:"discount"`       //优惠券
-	DiscountAmount string            `json:"discountAmount"` //优惠后价格
-	ThirdNo        string            `json:"thirdNo"`        //三方（购买订单）订单
-	CountryCode    string            `json:"countryCode"`    //国家代码
-	CityCode       string            `json:"cityCode"`       //城市代码
-	Refund         int               `json:"refund"`         //是否有退费 1存在退费
-	UpdatedAt      time.Time         `json:"updatedAt"`      //更新时间
-	Instance       []AppInstanceResp `json:"instances"`      //订单对应实列
+	OrderNo    string            `json:"orderNo"`    //平台订单号
+	AppOrderNo string            `json:"appOrderNo"` //渠道商（购买订单）订单号
+	Type       int8              `json:"type"`       //订单类型 1=新建 2=续费 3=释放
+	Status     int8              `json:"status"`     //订单状态 1=待处理 2=处理中 3=处理成功 4=处理失败 5=部分完成
+	Count      int               `json:"count"`      //购买数量
+	Amount     string            `json:"amount"`     //总价
+	Refund     int               `json:"refund"`     //是否有退费 1存在退费
+	Page       int               `json:"page"`       //页码 原样返回
+	PageSize   int               `json:"pageSize"`   //每页显示数量  原样返回
+	Total      int64             `json:"total"`      //订单对应实例总数量
+	Instances  []AppInstanceResp `json:"instances"`  //订单对应实例列表
 }
 
 // 获取实列列表请求
@@ -149,18 +147,18 @@ type AppGetInstanceReq struct {
 
 // 实例返回
 type AppInstanceResp struct {
-	InstanceNo  string    `json:"instanceNo"`  //平台实例编号（续费释放使用该编号）
+	InstanceNo  string    `json:"instanceNo"`  //平台实例编号（渠道商续费和释放操作使用该编号）
 	ProxyType   uint      `json:"proxyType"`   //代理类型 101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
-	Protocol    string    `json:"protocol" `   // 1=socks5 2=http 3=https 4=ssh
+	Protocol    string    `json:"protocol" `   //协议类型 多个用英文逗号分隔 1=socks5 2=http 3=https 4=ssh
 	Ip          string    `json:"ip"`          //代理ip地址
 	Port        uint      `json:"port"`        //代理端口
 	RegionId    string    `json:"regionId"`    //区域地址
 	CountryCode string    `json:"countryCode"` //国家代码
 	CityCode    string    `json:"cityCode"`    //城市代码
-	UseType     string    `json:"useType"`     //使用方式  1=账密 2=ip白名单 3=uuid（uuid写password内）
+	UseType     string    `json:"useType"`     //使用方式 多个用英文逗号分隔  1=账密 2=ip白名单 3=uuid（uuid写password内）
 	Username    string    `json:"username"`    //账户名或uuid 动态为平台主账号
 	Pwd         string    `json:"pwd"`         //密码
-	OrderNo     string    `json:"orderNo"`     //instance接口返回平台创建该实例的订单号 order接口返回操作该实例的订单号
+	OrderNo     string    `json:"orderNo"`     //创建该实例的平台订单号
 	UserExpired int64     `json:"userExpired"` //到期时间
 	FlowTotal   float64   `json:"flowTotal"`   //总流量
 	FlowBalance float64   `json:"flowBalance"` //剩余流量
@@ -168,8 +166,8 @@ type AppInstanceResp struct {
 	Renew       int8      `json:"renew"`       //1 自动续费
 	Bridges     []string  `json:"bridges"`     //桥地址列表
 	OpenAt      time.Time `json:"openAt"`      //开通时间
-	RenewAt     time.Time `json:"renewAt"`     //最后一次续费时间
-	ReleaseAt   time.Time `json:"releaseAt"`   //释放时间
+	RenewAt     time.Time `json:"renewAt"`     //最后成功续费时间
+	ReleaseAt   time.Time `json:"releaseAt"`   //释放成功时间
 }
 
 // 同步地域请求
@@ -187,40 +185,52 @@ type AppAreaResp struct {
 
 // 开通代理资源请求
 type AppInstanceOpenReq struct {
-	Params []OpenParam
+	AppOrderNo string      `json:"appOrderNo" form:"appOrderNo"` //购买者订单号(渠道商订单号)
+	Params     []OpenParam `json:"params" form:"params"`         //购买代理产品列表
 }
 
 type OpenParam struct {
-	ProductNo    string `json:"productNo" form:"productNo"`       //商品spu（如果存在spu，后面6项无意义）
-	ProxyType    uint16 `json:"proxyType" form:"proxyType"`       //代理类型 101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
-	CountryCode  string `json:"countryCode" form:"countryCode"`   //国家代码
-	CityCode     string `json:"cityCode" form:"cityCode"`         //城市代码
-	SupplierCode string `json:"supplierCode" form:"supplierCode"` //供应商代码（可为null,随机分配）
-	Unit         int8   `json:"unit" `                            //单位 1=天 2=周(7天) 3=月(自然月) 4=年(自然年365，366) 10=无限制
-	IspType      int    `json:"ispType" `                         //isp类型 1=单isp 2=双isp
-	OrderNo      string `json:"orderNo" form:"orderNo"`           //必要 购买发起方订单号必要，唯一
-	Count        int    `json:"count" form:"count"`               //购买数量 （实例个数）静态必填 默认1 一次最大20
-	Duration     int32  `json:"duration"`                         //必要 时长 默认1 为Unit的时长
-	Renew        bool   `json:"renew" `                           //是否续费 1续费 默认0
-	ExtBandWidth int32  `json:"extBandWidth"`                     //额外增加带宽 单位Mbps
-	Username     string `json:"username" form:"username"`         //平台主账号，选填 开通动态代理的时候平台主账号和渠道商主账号两个必填一个
-	AppUsername  string `json:"appUsername" form:"appUsername"`   //渠道商主账号，选填 开通动态代理的时候平台主账号和渠道商主账号两个必填一个
-	Flow         int    `json:"flow" form:"flow"`                 //动态流量 最大102400MB 动态必填 单位MB
-	UseBridge    uint8  `json:"useBridge"`                        //1=使用桥 2=不使用桥 3=随app设置 默认3
-	//新增 2024/06/27
-	CIDRBlocks []CIDRBlock `json:"cidrBlocks"`                 //静态购买所在网段及数量（产品有的才支持）
-	ProjectId  string      `json:"projectId" form:"projectId"` //购买项目id,保留字段，后续会支持
+	ProductNo    string      `json:"productNo" form:"productNo"`       //商品编号（如果存在，后面6项无意义）
+	ProxyType    uint16      `json:"proxyType" form:"proxyType"`       //代理类型 101=静态云平台 102=静态国内家庭 103=静态国外家庭 104=动态国外 105=动态国内 201=whatsapp
+	CountryCode  string      `json:"countryCode" form:"countryCode"`   //国家代码
+	CityCode     string      `json:"cityCode" form:"cityCode"`         //城市代码
+	SupplierCode string      `json:"supplierCode" form:"supplierCode"` //供应商代码（可为null,随机分配）
+	Unit         int8        `json:"unit"  form:"unit"`                //单位 1=天 2=周(7天) 3=月(自然月) 4=年(自然年365，366) 10=无限制
+	IspType      int         `json:"ispType"  form:"ispType"`          //isp类型 1=单isp 2=双isp
+	Count        int         `json:"count" form:"count"`               //购买数量 （实例个数）静态必填 默认1 一次最大20
+	Duration     int32       `json:"duration" form:"duration"`         //必要 时长 默认1 为Unit的时长
+	Renew        bool        `json:"renew"  form:"renew"`              //是否续费 1续费 默认0
+	ExtBandWidth int32       `json:"extBandWidth" form:"extBandWidth"` //额外增加带宽 单位Mbps
+	AppUsername  string      `json:"appUsername" form:"appUsername"`   //渠道商主账号，开通动态代理的时候必填(必须在平台上注册过)
+	Flow         int         `json:"flow" form:"flow"`                 //动态流量 最大102400MB 动态必填 单位MB
+	UseBridge    uint8       `json:"useBridge" form:"useBridge"`       //1=使用桥 2=不使用桥 3=随app设置 默认3
+	CIDRBlocks   []CIDRBlock `json:"cidrBlocks" form:"cidrBlocks"`     //静态购买所在网段及数量（产品有的才支持） 2024/06/27新增
+	ProjectId    string      `json:"projectId" form:"projectId"`       //购买项目id,保留字段，后续会支持
+}
+
+// 开通代理资源返回
+type AppInstanceOpenResp struct {
+	OrderNo    string `json:"orderNo"`    //平台订单号
+	AppOrderNo string `json:"appOrderNo"` //购买者订单号(渠道商订单号) 原样返回
+	Amount     string `json:"amount"`     //花费金额
 }
 
 // 续费代理资源请求
 type AppInstanceRenewReq struct {
-	OrderNo   string     `json:"orderNo" form:"orderNo"` //购买者订单号(渠道商订单号)
-	Instances []Instance `json:"instances"`              //实例列表
+	AppOrderNo string     `json:"appOrderNo" form:"appOrderNo"` //购买者订单号(渠道商订单号)
+	Instances  []Instance `json:"instances" form:"instances"`   //实例列表
 }
 
 type Instance struct {
-	Id       string `json:"idd"`      //平台实例编号
-	Duration int32  `json:"duration"` //可选 时长 默认1
+	InstanceNo string `json:"instanceNo" form:"instanceNo"` //平台实例编号
+	Duration   int32  `json:"duration" form:"duration"`     //可选 时长 默认1
+}
+
+// 续费代理资源返回
+type AppInstanceRenewResp struct {
+	OrderNo    string `json:"orderNo"`    //平台订单号
+	AppOrderNo string `json:"appOrderNo"` //购买者订单号(渠道商订单号) 原样返回
+	Amount     string `json:"amount"`     //花费金额
 }
 
 // 释放代理资源请求
@@ -231,10 +241,9 @@ type AppInstanceReleaseReq struct {
 
 // 释放代理资源返回
 type AppInstanceReleaseResp struct {
-	Status    int               `json:"status" form:"status"` //状态 1=待处理 2=处理中 3=处理成功 4=处理失败 5=部分完成
-	No        string            `json:"no"`                   //订单号（系统订单）
-	ThirdNo   string            `json:"thirdNo"`              //三方（购买订单）订单
-	Instances []AppInstanceResp `json:"instances"`            //订单对应实例列表
+	OrderNo    string `json:"orderNo"`    //平台订单号
+	AppOrderNo string `json:"appOrderNo"` //购买者订单号(渠道商订单号) 原样返回
+	Amount     string `json:"amount"`     //花费金额
 }
 
 // 账密提取请求
@@ -266,10 +275,10 @@ type AppProxyInfoReq struct {
 
 // 动态代理余额信息返回
 type AppProxyInfoResp struct {
-	Used        string   `json:"used" form:"used"`               //已使用
-	Total       string   `json:"total" form:"total"`             //总数
-	Balance     string   `json:"balance" form:"balance"`         //剩余
-	IpWhiteList []string `json:"ipWhiteList" form:"ipWhiteList"` //ip白名单
+	Used        string   `json:"used"`        //已使用
+	Total       string   `json:"total"`       //总数
+	Balance     string   `json:"balance"`     //剩余
+	IpWhiteList []string `json:"ipWhiteList"` //ip白名单
 }
 
 // 动态产品区域列表请求
@@ -311,7 +320,7 @@ type AppAddIpWhiteListReq struct {
 
 // 添加ip白名单返回
 type AppAddIpWhiteListResp struct {
-	IpWhiteList []string `json:"ipWhiteList" form:"ipWhiteList"` //ip白名单
+	IpWhiteList []string `json:"ipWhiteList"` //ip白名单
 }
 
 // 删除ip白名单
@@ -323,7 +332,7 @@ type AppDelIpWhiteListReq struct {
 
 // 删除ip白名单返回
 type AppDelIpWhiteListResp struct {
-	IpWhiteList []string `json:"ipWhiteList" form:"ipWhiteList"` //ip白名单
+	IpWhiteList []string `json:"ipWhiteList"` //ip白名单
 }
 
 // Api提取代理请求
